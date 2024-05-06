9047e182423e39b9814c4b4d29c488c9fbd1269e
Norxz <espinosalozano6819@gmail.com>
2024-05-05 21:42:56 -0500

    hace algo

--- Bible/src/main/Interfaz.java
+++ Bible/src/main/Interfaz.java
@@ -1,6 +1,7 @@
 
 package main;
 
+import Files.LimpiarTexto;
 /**
  *
  * @author lokci
@@ -11,9 +12,11 @@ public class Interfaz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        
+        String rutaEntrada = "C:\\Users\\espin\\Documents\\NetBeansProjects\\Biblia\\Bible\\src\\Files\\Biblia.txt";
+        String rutaSalida = "C:\\Users\\espin\\Documents\\NetBeansProjects\\Biblia\\Bible\\src\\Files\\biblia_limpiada.txt";
+
+        LimpiarTexto.limpiarArchivo(rutaEntrada, rutaSalida);
+                
     }
     
 }
